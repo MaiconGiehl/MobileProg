d4058a5b51b3943843e485ab5bb5be880f1409b6
Maicon Giehl <maicongiehl@gmail.com>
2022-07-09 23:52:39 -0300

    Counter

--- App.tsx
+++ App.tsx
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
 
-export default function App() {
+// import { Header } from "react-native";
+
+const Header = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.headerBox}>
+      <Text style={styles.headerText}>Contador</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+let styles = StyleSheet.create({
+  headerBox: {
+    height: 60,
+    backgroundColor: '#4867D7'
   },
+  headerText: {
+    padding: 15,
+    fontSize: 20,
+    color: '#fff'
+  },
+  counterText: {
+    fontSize: 30,
+    color: '#234',
+    textAlign: 'center',
+    paddingTop: 150,
+    paddingBottom: 40
+  },
+  button: {
+    width: 200,
+    margin: 'auto'  
+  }
 });
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <View>
+      <Header />
+      <Text style={styles.counterText}>{counter}</Text>
+      <View style={styles.button}>
+        <Button title="Clique aqui" onPress={(() => { setCounter(counter + 1) })}></Button>
+      </View>
+    </View>
+  );
+};
+
+const App = () => {
+  return (
+    <View>
+      <Counter />
+      <Text>{}</Text>
+    </View>
+  );
+};
+
+export default App;
